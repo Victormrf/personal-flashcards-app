baf16c1488302aff6e2a55e8220a6c135f2cda86
Victormrf <vmrf2000@hotmail.com>
2026-07-05 01:34:55 -0300

    feat: add Redis cache for due cards with cache-aside pattern

---
+++ api/internal/cache/redis.go
@@ -0,0 +1,40 @@
+// Redis client wrapper
+
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisCache struct {
+	client *redis.Client
+}
+
+func NewRedisCache(url string) *RedisCache {
+	opts, err := redis.ParseURL(url)
+	if err != nil {
+		panic("invalid redis url: " + err.Error())
+	}
+	return &RedisCache{client: redis.NewClient(opts)}
+}
+
+func (c *RedisCache) Get(ctx context.Context, key string) (string, error) {
+	return c.client.Get(ctx, key).Result()
+}
+
+func (c *RedisCache) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
+	return c.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (c *RedisCache) Delete(ctx context.Context, key string) error {
+	return c.client.Del(ctx, key).Err()
+}
+
+func (c *RedisCache) Ping(ctx context.Context) error {
+	return c.client.Ping(ctx).Err()
+}
+
+
